e2ec8c5c95f088168b7db6d879dc5a26f17d58f0
NHJ-macos <jwang0702@foxmail.com>
2022-03-02 15:32:39 +0800

    v2.0

--- tomato-clock/tomato-clock.py
+++ tomato-clock/tomato-clock.py
@@ -9,6 +9,9 @@
 
 import time
 import os
+import subprocess
+from pydub import AudioSegment
+from pydub.playback import play
 
 # =============================
 
@@ -21,14 +24,28 @@ skipTimes = 0 #跳过的休息次数
 cur = 1
 t1 = time.mktime(curTime)
 t2 = time.mktime(beginTime)
+song = AudioSegment.from_wav("sound.wav")
+
 
 # =============================
+
+# macOS notification===========
+
+def show_notification(title, text):
+    cmd = 'display notification \"' + \
+          text + '\" with title \"' + title + '\"'
+    subprocess.call(["osascript", "-e", cmd])
+    play(song)
+
+# =============================
+
 
 os.system('clear')  # macOS
 print ("This is tomato clock, enjoy studying!\n")
 while cur:
     if input("If you are ready to study, please press enter!\n") == '':
         numClocks = numClocks + 1
+        show_notification("Study Begins!", "The No." + str(numClocks) + " clock starts.")
         beginTime = time.localtime(time.time())                     # 记录开始时间
         tB = time.mktime(beginTime)
         tC = time.mktime(curTime)
@@ -40,14 +57,16 @@ while cur:
             print('This is the No.'+ str(numClocks) +' clock!')
             print('{0}  mins  {1} secs remaining!'.format(24-diff.tm_min,59-diff.tm_sec))
             time.sleep(0.98)
-
+        show_notification("Well Done!", "The No."+ str(numClocks) + " clock is done.")
         print('\n\nCongratulations! The No.'+ str(numClocks) + ' clock is done.\n\n')
         time.sleep(2)
         if input("Start resting, please press enter!\nSkip rest (which would be accumulated), please enter the other:\n") == '':
+
             beginTime = time.localtime(time.time())
             tB = time.mktime(beginTime)
             tC = time.mktime(curTime)
             breakTime = skipTimes * 5 * 60 + 5 * 60  # accumulate break time
+            show_notification("Have A Break!", "You can rest for " + str(skipTimes * 5 + 5) + " mins!" )
             skipTimes = 0
             while tC - tB <= breakTime:
                 curTime = time.localtime(time.time())
@@ -57,6 +76,7 @@ while cur:
                 print('Rest time!')
                 print('{0}  mins  {1} secs remaining!'.format(skipTimes * 5 + 5 - 1 - diff.tm_min, 59 - diff.tm_sec))
                 time.sleep(0.98)
+            show_notification("Let's Study!", "Break time is over!")
             print('\n\nBreak time is done! Clock will re-startup in 6 secs.')
             time.sleep(6)
             os.system('clear')  # macOS
